507d7d7917a3a111fd2b2843b2c5acdc19f9b8d2
Brian Frick <KrumphauChicken@users.noreply.github.com>
2017-03-22 12:38:32 -0700

    Created main.cpp and example function.

---
+++ headers/frick.h
@@ -0,0 +1,8 @@
+// Name: Brian Frick
+// Function: Print an example
+
+#include <iostream>
+
+void print_an_example(){
+	std::cout<<"This is an example.\n";
+}
---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include "headers/frick.h"
+
+using namespace std;
+
+int main()
+{
+	short unsigned int choice;
+	cout<<\
+	"0) Print \"This is an example.\"\
+	\n1) \
+	\n2) \
+	\n3) \
+	\n4) \
+	\n5) \
+	\n6) \
+	\n7) \
+	\n8) \
+	\n9) \
+	\n10) \
+	\n11) \
+	\n12) \
+	\n13) \
+	\n14) \
+	\n15) \
+	\n16) \
+	\n17) \
+	\n18) \
+	\n";
+	cout<<"Please pick an option: ";
+	cin>>choice;
+	
+	switch (choice) {
+		case 0: print_an_example(); break;
+		case 1: break;
+		case 2: break;
+		case 3: break;
+		case 4: break;
+		case 5: break;
+		case 6: break;
+		case 7: break;
+		case 8: break;
+		case 9: break;
+		case 10: break;
+		case 11: break;
+		case 12: break;
+		case 13: break;
+		case 14: break;
+		case 15: break;
+		case 16: break;
+		case 17: break;
+		case 18: break;
+	}
+	
+	return 0;
+}
