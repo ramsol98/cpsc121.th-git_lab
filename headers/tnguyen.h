be6c10ac09dc3b4e4eb5963255f5891ee7d1998e
tuann1821 <tuann1821@csu.fullerton.edu>
2017-03-26 13:19:29 -0700

    Update tnguyen.h

--- headers/tnguyen.h
+++ headers/tnguyen.h
@@ -1,5 +1,14 @@
 //Name: Tuan-Anh Nguyen
-//Print function
+//Function: surfaceArea_of_Cube
 
 #include <iostream>
-int findarea(int length, int width, int height);
+
+void surfaceArea_of_Cube()
+{
+  float side, surfaceArea;
+  std::cout<<"What is the length of a side?"<<endl;
+  std::cin>>side;
+  surfaceArea = 6.0*pow(side,2);
+  std::cout<<"The surface area of a cube is"<<surfaceArea<<endl;
+}
+  
